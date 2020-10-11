54c01fe273039bd4ca1953261d0efe2a0b22c94e
Phil <12659478+TheRealHypR@users.noreply.github.com>
2020-10-11 14:44:11 +0200

    Create myfreefarm-helper.user.js

---
+++ myfreefarm-helper.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         MyFreeFarm Helper
+// @namespace    https://github.com/TheRealHypR/myfreefarm-userscripts
+// @homepageURL  https://github.com/TheRealHypR/myfreefarm-userscripts/
+// @updateURL    https://github.com/TheRealHypR/myfreefarm-userscripts/raw/master/myfreefarm-helper.user.js
+// @downloadURL  https://github.com/TheRealHypR/myfreefarm-userscripts/raw/master/myfreefarm-helper.user.js
+// @version      0.1
+// @description  a collection of scripts to help you play myfreefarm
+// @author       TheRealHypR
+// @match        http://s*.myfreefarm.de/*
+// @grant        all
+// @require      http://code.jquery.com/jquery-3.5.1.min.js
+// @run-at       document-end
+// ==/UserScript==
+
